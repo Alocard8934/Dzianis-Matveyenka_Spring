6b087141258ffdf2943026eb1756401d404c1280
Alocard8934 <yuriy1989@list.ru>
2025-08-08 22:54:20 +0200

    08-08-2025 22:54

--- app/src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ app/src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -1,39 +1,14 @@
 package com.dmdev.spring;
 
-import com.dmdev.spring.config.ApplicationConfiguration;
-import com.dmdev.spring.database.pool.ConnectionPool;
-import com.dmdev.spring.database.repository.CompanyRepository;
-import com.dmdev.spring.database.repository.CrudRepository;
-import com.dmdev.spring.database.repository.UserRepository;
-import com.dmdev.spring.ioc.Container;
-import com.dmdev.spring.service.CompanyService;
-import com.dmdev.spring.service.UserService;
-import java.io.Serializable;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@SpringBootApplication
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        String value = "hello";
-        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
-        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
-        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
-
-        try (var context = new AnnotationConfigApplicationContext()) {
-            context.register(ApplicationConfiguration.class);
-            context.getEnvironment().setActiveProfiles("web", "prod");
-            context.refresh();
-
-            var connectionPool = context.getBean("pool1", ConnectionPool.class);
-
-            System.out.println(connectionPool);
-            /*var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
-            System.out.println(companyRepository);*/
-            var companyService = context.getBean(CompanyService.class);
-            System.out.println(companyService.findById(1));
-
-        }
-    }
+        SpringApplication.run(ApplicationRunner.class, args);
+    
+    }   
 }
